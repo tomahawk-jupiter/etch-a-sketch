294d8a043d20c8caaea58389de80e5842f2bd506
jupiter <brianbrainrogers@gmail.com>
2022-05-21 15:37:01 +0100

    Improved cell layout

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Etch a Sketch Project (from Odin Project)
+# Etch a Sketch Project
 
-A poject to practice DOM manipulation.
+A project to practice vanilla JS DOM manipulation. An Odin Project assignment.
+
+[Live Demo](https://tomahawk-jupiter.github.io/etch-a-sketch/)
+
+## Overview
+
+An Etch a Sketch style drawing board. Move the mouse over the canvas area to draw on it.
+
+A resolution can be selected for smaller cell sizes allowing more detail.
+
+## Board and Cell Size
+
+Set the `canvasWidth` variable in `index.js` for the size of the board.
+
+Don't actually set the size of the `.canvas-container` div with this value it will be too small to fit the cells and will be hidden behind them (I don't know why this is). The div will resize to fit the contents by itself.
+
+This means the board won't be the exact size of the `canvasWidth` value but will be close.
+
+Divide the canvas width by the number of cells to get the width / height for each cell.
+
+## Appending the Cells with a loop
+
+Use a nested loop. The first loop creates a row, the second loop adds the column cells for the row. At the end of each row iteration, it is appended to the Canvas.
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const canvasWidth = 470;
 const initialCells = 16;
-const canvasColor = '#cdaa87';
-const inkColor = '#46290b';
+const canvasColor = "#cdaa87";
+const inkInitialColor = "#46290b";
+let inkColor = "#46290b";
 
-let canvasContainer = document.querySelector('.canvas-container');
-canvasContainer.setAttribute('style', `width:${canvasWidth}px;
-    height:${canvasWidth}px;`);
+let canvasContainer = document.querySelector(".canvas-container");
 
-function drawCanvas(cellsPerSide){
-  const totalCells = cellsPerSide * cellsPerSide;
+function drawCanvas(cellsPerSide) {
   const cellWidth = canvasWidth / cellsPerSide;
 
-  for (let i = 0; i < totalCells; i++){
-    let cell = document.createElement('div');
-    cell.setAttribute('class', 'cell');
-    cell.setAttribute(
-      'style', `width:${cellWidth}px;height:${cellWidth}px;
+  for (let row = 0; row < cellsPerSide; row++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+
+    for (let col = 0; col < cellsPerSide; col++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      cell.setAttribute(
+        "style",
+        `width:${cellWidth}px;height:${cellWidth}px;
       background-color:${canvasColor};`
-    );
-    cell.addEventListener('mouseover', e => {
-      e.target.style['background-color'] = inkColor;
-    });
-    canvasContainer.appendChild(cell);
+      );
+
+      // This event listener is what allows drawing //
+      cell.addEventListener("mouseover", (e) => {
+        e.target.style["background-color"] = inkColor;
+      });
+      row.appendChild(cell);
+    }
+    canvasContainer.appendChild(row);
   }
 }
 
@@ -29,31 +36,46 @@ function drawCanvas(cellsPerSide){
 drawCanvas(initialCells);
 
 // Wipes the canvas clean.
-function clear(){
-  let cells = document.querySelectorAll('.cell');
+function clear() {
   canvasContainer.replaceChildren();
+  inkColor = inkInitialColor;
 }
 
-let buttonResolution = document.querySelector('.btn-resolution');
-let buttonClear = document.querySelector('.btn-clear');
+let buttonResolution = document.querySelector(".btn-resolution");
+let buttonClear = document.querySelector(".btn-clear");
 let userCellsPerSide = initialCells;
 
 // Clear canvas and change resolution.
-buttonResolution.addEventListener('click', ()=> {
-  userCellsPerSide = prompt(
-    'Choose number of cells per side (max 99)'
-  );
+buttonResolution.addEventListener("click", () => {
+  userCellsPerSide = prompt("Choose number of cells per side (max 99)");
+
+  // Clear old board before rendering new one
   clear();
-  if (userCellsPerSide > 100){
-    alert('Number too high! Default setting will be applied.');
+  if (!userCellsPerSide) {
+    alert("No value entered! Default will be applied");
     drawCanvas(initialCells);
-  } else if (userCellsPerSide < 100){
+  }
+  if (userCellsPerSide > 100) {
+    alert("Number too high! Default setting will be applied.");
+    drawCanvas(initialCells);
+  }
+  if (userCellsPerSide < 100 && userCellsPerSide > 0) {
     drawCanvas(userCellsPerSide);
   }
 });
 
 // Clear canvas and keep resolution the same.
-buttonClear.addEventListener('click', ()=>{
+buttonClear.addEventListener("click", () => {
   clear();
   drawCanvas(userCellsPerSide);
+});
+
+const eraser = document.getElementById("btn-eraser");
+
+eraser.addEventListener("click", () => {
+  if (inkColor === inkInitialColor) {
+    inkColor = canvasColor;
+    return;
+  }
+  inkColor = inkInitialColor;
 });
